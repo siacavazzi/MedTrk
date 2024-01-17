9ee594eb158143a27aeb1b066c404334905190a7
Sam <siacavazzi@gmail.com>
2024-01-17 16:05:31 -0500

    added header

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header({user, setUser}) {
 
     return (
         <div>
-            
+            {user ? <p> Welcome {user.name}!</p>: <p>Please sign in</p>}
         </div>
     )
 }
